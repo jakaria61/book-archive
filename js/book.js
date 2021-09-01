228792f9b56795adf7f96b8a7fa614dc29a1111b
jakaria61 <bdjakaria629@gmail.com>
2021-09-02 03:15:18 +0600

    if else update

--- js/book.js
+++ js/book.js
@@ -1,8 +1,6 @@
 
 const errorDiv = document.getElementById('error');
 errorDiv.style.display = 'none';
-
-
 const searchBook = () => {
     const inputValue = document.getElementById('input-filed');
     const searchText = inputValue.value;
@@ -21,8 +19,9 @@ const searchBook = () => {
 
 const showBook = (books) => {
     console.log(books.length);
-    if (books.length === 0) {
-        errorDiv.innerHTML = `<h1>No Result Found</h1>`;
+    if (parseInt(books.length) === 0) {
+        errorDiv.style.display = 'block';
+        errorDiv.innerText = 'no result found';
 
     }
     else {
@@ -30,13 +29,17 @@ const showBook = (books) => {
         const bookContainer = document.getElementById('book-container');
         bookContainer.textContent = '';
         books.forEach(book => {
+            let author = '';
+            let bookPublisher = '';
             const div = document.createElement('div');
             div.classList.add('col');
+            book.author_name ? author = book.author_name[0] : '';
+            book.publisher ? bookPublisher = book.publisher[0] : '';
             div.innerHTML = `
              <img src='https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg'/>
              <h4>${book.title}</h4>
-             <p>author: ${book.author_name[0]}</p>
-             <p>publiser: ${book.publisher[0]}</p>
+             <p>author: ${author}</p>
+             <p>publiser: ${bookPublisher}</p>
              <p>frist publist: ${book.first_publish_year}</p>
              
         `;
